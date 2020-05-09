e2738ff3b896e918a7307c77ec146f296fd45f5a
olegvolkov <wwwolkov@yahoo.co.uk>
2020-05-09 13:53:29 +0100

    Dos

---
+++ src/app/ui-imports.module.ts
@@ -0,0 +1,110 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NouisliderModule } from 'ng2-nouislider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PlotlyModule } from 'angular-plotly.js';
+import { NgPipesModule } from 'ngx-pipes';
+import { InputTrimModule } from 'ng2-trim-directive';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NouisliderModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    MatIconModule,
+    MatRadioModule,
+    MatButtonModule,
+    FormsModule,
+    MatListModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatTableModule,
+    MatDialogModule,
+    PlotlyModule,
+    MatGridListModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatSlideToggleModule,
+    MatProgressBarModule,
+    NgPipesModule,
+    InputTrimModule,
+    MatPaginatorModule,
+    MatRippleModule,
+    MatSliderModule,
+    ColorPickerModule,
+    MatButtonToggleModule,
+    MatBadgeModule,
+    MatMenuModule,
+    MatTooltipModule,
+    DragDropModule
+
+
+  ],
+  exports: [
+    NouisliderModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    MatIconModule,
+    MatRadioModule,
+    MatButtonModule,
+    FormsModule,
+    MatListModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatTableModule,
+    MatDialogModule,
+    PlotlyModule,
+    MatGridListModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatSlideToggleModule,
+    MatProgressBarModule,
+    NgPipesModule,
+    InputTrimModule,
+    MatPaginatorModule,
+    MatRippleModule,
+    MatSliderModule,
+    ColorPickerModule,
+    MatButtonToggleModule,
+MatBadgeModule,
+    MatMenuModule,
+    MatTooltipModule,
+DragDropModule,
+
+
+  ]
+})
+export class UiImportsModule { }
